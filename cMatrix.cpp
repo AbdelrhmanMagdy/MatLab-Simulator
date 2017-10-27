de62a9b565d2e7426b9cfd6c4ecd0a8726eb724a
AbdelrhmanMagdy <abdelrhman.magdy96@gmail.com>
2017-10-28 00:30:46 +0200

    constructor, destructor, reset

--- cMatrix.cpp
+++ cMatrix.cpp
@@ -2,4 +2,23 @@
 using namespace std;
 
 CMatrix::CMatrix(){
+    nR = nC = 0;
+    values = NULL;
+}
+CMatrix::CMatrix(string s){
+    nR = nC =0;
+    values = NULL;
+    copy(s);
+}
+CMatrix::copy(string s){
+    reset();
+    
+}
+CMatrix::reset(){
+    if (values){
+        for (int i=0; i<nR; i++) delete[] values[i];
+        delete[] vlaues;
+    }
+    nR = nC = 0;
+    values = NULL;
 }
--- main.cpp
+++ main.cpp
@@ -7,6 +7,8 @@ using namespace std;
 
 int main()
 {
+
+    
     
 
 return 0;
